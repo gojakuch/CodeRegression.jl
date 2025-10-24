4c00b15a3a57862c03ab91eadda5b9a793db87cf
Atell Krasnopolski <delta_atell@protonmail.com>
2025-10-24 22:00:44 +0200

    Proof of concept example (sign function search)

---
+++ examples/finding_functions.jl
@@ -0,0 +1,74 @@
+using Random
+include("../merging.jl")
+include("../mutating.jl")
+
+target_f = sign
+
+function objective_precompile(candidates::Vector{Pair{Expr, Float32}})
+    pairs_and_functions = Dict{Int, Function}()
+
+    for i in eachindex(candidates)
+        def, l = candidates[i]
+        if isnan(l)
+            f = eval(def)
+            precompile(f, (Float32,))
+            pairs_and_functions[i] = function (x)
+                try
+                    return f(x)
+                catch er
+                    return Inf
+                end
+            end
+        end
+    end
+
+    pairs_and_functions
+end
+
+function objective!(candidates::Vector{Pair{Expr, Float32}}, pairs_and_functions::Dict{Int, Function}, N = 100)
+    points = rand(N) .* 2 .- 1
+    push!(points, 0)
+
+    for (i, f) in pairs_and_functions
+        candidates[i] = Pair{Expr, Float32}(candidates[i][1], sum(abs.(target_f.(points) .- f.(points))) / (N+1)) # MAE
+    end
+end
+
+init_f = :(function (x)
+        return 1
+    end);
+candidates = Pair{Expr, Float32}[Pair(init_f, NaN)];
+max_size = 50;
+trim_size = 20;
+iters = 3;
+reproducing_pairs = 5;
+
+for it in 1:iters
+    # mutate
+    mutpair(p) = Pair{Expr, Float32}(mutate(p[1]), NaN)
+    mutants = mutpair.(candidates)
+    candidates = cat(candidates, mutants; dims=1)
+
+    # reproduce
+    # TODO: maybe figure out a good distribution for how to pick the reproducing pairs, for the best to be ahead??
+    children = Pair{Expr, Float32}[]
+    for _ in 1:reproducing_pairs
+        shuffle!(candidates)
+        parents = candidates[1:2]
+        try # FIXME: remove and check for errors??
+        push!(children, (reproduce(parents[1][1], parents[2][1]) => NaN32)) # possible duplicates
+        catch
+        end
+    end
+    candidates = cat(candidates, children; dims=1)
+
+    # compute objectives and sort
+    pf = objective_precompile(candidates)
+    objective!(candidates, pf)
+    sort!(candidates; lt=(x, y)->(isless(x[2], y[2])))
+    if length(candidates) > max_size
+        candidates = candidates[1:trim_size]
+    end
+end
+
+candidates[1]
