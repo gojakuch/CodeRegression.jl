29696c72221d699f410457bd0d323d7e1de1c4e2
Atell Krasnopolski <delta_atell@protonmail.com>
2025-10-24 22:00:08 +0200

    A very bad prototype of mutate

--- generating.jl
+++ generating.jl
@@ -1,18 +1,27 @@
 include("utils.jl")
 
-function generate_regression(f::Expr)
-    all_exprs = get_args(f)
+# TODO: generate statements, LHS, and RHS expression lists separately. manage these lists properly so that this helps to achieve the optimum
 
-    for i in 1:10
-        r = rand()
-        derive_new_expr!(all_exprs, r)
+function generate_regression(f::Expr, iters=10)
+    check_expr_type(f, :function)
+
+    all_exprs = get_args(f)
+    # TODO: values!!
+    push!(all_exprs, :0)
+    push!(all_exprs, :1)
+    push!(all_exprs, :(-1)) # FIXME: kinda cheating for now. should be generated from 1 naturally via the RHS mutator
+
+    for i in 1:iters
+        derive_new_expr!(all_exprs, f, rand())
     end
 
     all_exprs
 end
 
-function derive_new_expr!(all_exprs::Vector, r)
+function derive_new_expr!(all_exprs::Vector, f, r)
     if(r < 0.5)
+        push!(all_exprs, generate_assignment(all_exprs, f))
+    elseif(r < 0.75)
         push!(all_exprs, generate_bin_op(all_exprs))
     elseif r < 0.95
         push!(all_exprs, generate_return(all_exprs))
@@ -21,12 +30,22 @@ function derive_new_expr!(all_exprs::Vector, r)
     end
 end
 
+function generate_assignment(all_exprs::Vector, f::Expr)
+    # TODO: we should accept 2 different arrays: for conds and for bodies generated separately if we want to continue using this approach. filtering is inefficient. 
+    candidates = filter(x -> !(typeof(x)==Expr && (x.head == :if || x.head == :return)), all_exprs) # TODO remove typeof
+    Expr(
+        :(=),
+        rand(get_args(f)),
+        rand(candidates)
+    )
+end
+
 function generate_bin_op(all_exprs::Vector, ConstProb = 0.5, BinProb = 0.5)
     r = rand()
 
     CurrProb = ConstProb
     if r < CurrProb
-        candidates = filter(x -> !(typeof(x)==Expr && x.head == :if), all_exprs) # TODO remove typeofs in all those filters
+        candidates = filter(x -> !(typeof(x)==Expr && (x.head == :if || x.head == :return || x.head == :(=))), all_exprs) # TODO remove typeofs in all those filters. actually, remove the filters entirely, only accept arrays with what's acceptable
         if isempty(candidates)
             return :(nothing)
         end
@@ -36,23 +55,26 @@ function generate_bin_op(all_exprs::Vector, ConstProb = 0.5, BinProb = 0.5)
     CurrProb += BinProb
     if r < CurrProb
         r_norm = (r - CurrProb) / BinProb + 1
-        if r_norm < 0.5
-            ex = :(nothing > nothing)
+        if r_norm < 1/3
+            ex = :(0 > 0)
+        elseif r_norm < 2/3
+            ex = :(0 == 0)
         else
-            ex = :(nothing < nothing)
+            ex = :(0 < 0)
         end
         ConstProb, BinProb = ConstProb + BinProb / 2, BinProb / 2
         left = generate_bin_op(all_exprs, ConstProb, BinProb)
-        ex.args[2] = (typeof(left) == Expr) ? left.args[1] : left # TODO remove typeof
+        ex.args[2] = left #(typeof(left) == Expr) ? left.args[end] : left # TODO remove typeof
         right = generate_bin_op(all_exprs, ConstProb, BinProb)
-        ex.args[3] = (typeof(right) == Expr) ? right.args[1] : right # TODO remove typeof
+        ex.args[3] = right #(typeof(right) == Expr) ? right.args[end] : right # TODO remove typeof
     end
 
     return ex
 end
 
 function generate_return(all_exprs::Vector)
-    candidates = filter(x -> !(typeof(x)==Expr && (x.head == :if || x.head == :return)), all_exprs) # TODO remove typeof
+    # TODO: we should accept 2 different arrays: for conds and for bodies generated separately if we want to continue using this approach. filtering is inefficient. 
+    candidates = filter(x -> !(typeof(x)==Expr && (x.head == :if || x.head == :return || x.head == :(=))), all_exprs) # TODO remove typeof
     if isempty(candidates)
         return :(nothing)
     end
@@ -61,6 +83,7 @@ function generate_return(all_exprs::Vector)
 end
 
 function generate_if(all_exprs::Vector)
+    # TODO: we should accept 2 different arrays: for conds and for bodies generated separately if we want to continue using this approach. filtering is inefficient. 
     candidates_cond = filter(x -> !(typeof(x)==Expr && (x.head == :if || x.head == :return)), all_exprs) # TODO remove typeof
     candidates_body = filter(x -> !(typeof(x)==Expr && x.head == :if), all_exprs) # TODO remove typeof
     if isempty(candidates_body)
--- mutating.jl
+++ mutating.jl
@@ -1,13 +1,97 @@
 include("generating.jl")
 
-function mutate(f::Expr)
+function mutate(f::Expr)::Expr
+    # FIXME: this is a prototype!!! this should NOT go any further than the proof of concept
+    # TODO: I think we need to replace this with a modifying visitor that finds what to change recursively and that has modular system (meaning the user can turn on and off some possible changes or supply custom ones)
+
     check_expr_type(f, :function)
 
-    r = rand()
+    new_f = deepcopy(f)
+    exprs = generate_regression(f, 1)
 
-    # TODO
-    if r < 0.25
-        # just add smth
-        
+    function insertexpr!(arr)
+        r2 = rand()
+        ex = :(nothing)
+        if r2 < 0.2
+            ex = generate_assignment(exprs, new_f)
+        else
+            ex = generate_if(exprs)
+        end
+
+        if length(arr) > 1
+            insert!(arr, rand(1:(length(arr)-1)), ex)
+        else
+            insert!(arr, 1, ex)
+        end
     end
+
+    r = rand()
+    if r < 0.4
+        # just add smth before the return (random line)
+        insertexpr!(get_body(new_f).args)
+    elseif r < 0.75
+        # modify something (again, we'll need to do this recursively, it's dumb not to)
+        # do we need to modify the return in the end?
+
+        inds = filter(i -> (typeof(get_body(new_f).args[i]) == Expr), 1:(length(get_body(new_f).args)-1))
+        if isempty(inds)
+            insertexpr!(get_body(new_f).args)
+            return new_f
+        end
+        ind = rand(inds)
+        ex_i = get_body(new_f).args[ind]
+
+        if ex_i.head == :if 
+            # choose if we modify the body or cond
+            # recursion here later
+            r2 = rand()
+            if r2 < 1/3
+                # modify cond
+                ex_i.args[1] = rand(filter(x -> !(typeof(x)==Expr && (x.head == :if || x.head == :return)), exprs))
+            else
+                # insert to the block
+                i = 2 + (length(ex_i.args) > 2 && rand() > 0.5) # decide if we append to the if or to the else
+                if typeof(ex_i.args[i]) != Expr
+                    ex_i.args[i] = Expr(:block, ex_i.args[i])
+                end
+                insertexpr!(ex_i.args[i].args)
+                # r2 = rand()
+                # ex = :(nothing)
+                # if r2 < 0.5
+                #     ex = generate_assignment(exprs, new_f)
+                # else
+                #     ex = generate_if(exprs)
+                # end
+
+                # insert!(ex_i.args[i].args, rand(1:(length(ex_i.args[i].args)-1)), ex)
+            end
+        elseif ex_i.head == :(=)
+            # choose the side
+            r2 = 1
+            if length(get_args(f)) > 1 # check if there are other variables to assign to
+                r2 = rand()
+            end
+            if r2 < 0.3
+                # lhs
+                ex_i.args[1] = rand(get_args(f))
+            else
+                # TODO: separate recursion for the rhs. potential SR.jl integration here (optional)
+                var = ex_i.args[1]
+                ex_i = generate_assignment(exprs, new_f)
+                ex_i.args[1] = var
+                get_body(new_f).args[ind] = ex_i
+            end
+        end
+    else
+        # remove something
+        inds = filter(i -> (typeof(get_body(new_f).args[i]) != LineNumberNode), 1:(length(get_body(new_f).args)-1))
+        if isempty(inds)
+            insertexpr!(get_body(new_f).args)
+            return new_f
+        end
+        ind = rand(inds)
+        deleteat!(get_body(new_f).args, ind)
+    end
+
+    return new_f
 end
