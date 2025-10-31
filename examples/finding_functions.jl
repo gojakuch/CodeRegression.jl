be0b392cc53e1ec356155b19f396f7132956ce0e
Atell Krasnopolski <delta_atell@protonmail.com>
2025-10-31 13:37:31 +0100

    demand the return type from the user and forbid nested functions

--- examples/finding_functions.jl
+++ examples/finding_functions.jl
@@ -4,27 +4,17 @@ include("../core/mutating.jl")
 
 target_f = sign
 
-function objective_precompile(candidates::Vector{Pair{Expr, Float32}})
+function objective_precompile(candidates::Vector{Pair{Expr, Float64}}, par_types::Tuple)
     pairs_and_functions = Dict{Int, Function}()
 
     for i in eachindex(candidates)
         def, l = candidates[i]
         if isnan(l)
-            dump(def)
-            print(def)
-
             f = x -> Inf
-            # try # TODO: REMOVE THAT AFTER FIXING THE ISSUES
-                f = eval(def)
-            # catch er
-            # end
-            precompile(f, (Float32,))
+            f = eval(def)e
+            precompile(f, par_types)
             pairs_and_functions[i] = function (x)
-                # try # TODO: REMOVE THAT AFTER FIXING THE ISSUES
-                    return f(x)
-                # catch er
-                #     return Inf
-                # end
+                return f(x)
             end
         end
     end
@@ -32,45 +22,48 @@ function objective_precompile(candidates::Vector{Pair{Expr, Float32}})
     pairs_and_functions
 end
 
-function objective!(candidates::Vector{Pair{Expr, Float32}}, pairs_and_functions::Dict{Int, Function}, N = 100)
+function objective!(candidates::Vector{Pair{Expr, Float64}}, pairs_and_functions::Dict{Int, Function}, N = 100)
     points = rand(N) .* 2 .- 1
     push!(points, 0)
 
     for (i, f) in pairs_and_functions
-        candidates[i] = Pair{Expr, Float32}(candidates[i][1], sum(abs.(target_f.(points) .- f.(points))) / (N+1)) # MAE
+        candidates[i] = Pair{Expr, Float64}(candidates[i][1], sum(abs.(target_f.(points) .- f.(points))) / (N+1)) # MAE
     end
 end
 
-init_f = :(function (x::Float32)
+init_f = :(function (x::Float64)
         return 1
     end);
-candidates = Pair{Expr, Float32}[Pair(init_f, NaN)];
+par_types = (Float64,);
+return_type = Float64;
+candidates = Pair{Expr, Float64}[Pair(init_f, NaN)];
 max_size = 50;
 trim_size = 10;
 iters = 1;
 reproducing_pairs = 4;
+gen_depth = 1;
 
 for it in 1:iters
     # mutate
-    mutpair(p) = Pair{Expr, Float32}(mutate(p[1]), NaN)
+    mutpair(p) = Pair{Expr, Float64}(mutate(p[1], return_type, gen_depth), NaN64)
     mutants = mutpair.(candidates)
     candidates = cat(candidates, mutants; dims=1)
 
     # reproduce
     # TODO: maybe figure out a good distribution for how to pick the reproducing pairs, for the best to be ahead??
-    children = Pair{Expr, Float32}[]
+    children = Pair{Expr, Float64}[]
     for _ in 1:reproducing_pairs
         shuffle!(candidates)
         parents = candidates[1:2]
         try # FIXME: remove and check for errors??
-        push!(children, (reproduce(parents[1][1], parents[2][1]) => NaN32)) # possible duplicates
+        push!(children, (reproduce(parents[1][1], parents[2][1]) => NaN64)) # possible duplicates
         catch
         end
     end
     candidates = cat(candidates, children; dims=1)
 
     # compute objectives and sort
-    pf = objective_precompile(candidates)
+    pf = objective_precompile(candidates, par_types)
     objective!(candidates, pf)
     sort!(candidates; lt=(x, y)->(isless(x[2], y[2])))
     if length(candidates) > max_size
