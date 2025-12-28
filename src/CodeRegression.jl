e8f3d336344d65b443ab97e1ce81d96b347e3bed
Atell Krasnopolski <delta_atell@protonmail.com>
2025-12-28 22:27:07 +0100

    rename `generating.jl` and `mutating.jl` to proper forms

--- src/CodeRegression.jl
+++ src/CodeRegression.jl
@@ -8,8 +8,8 @@ include("literal_visitor.jl");
 include("constructors.jl");
 include("init.jl");
 include("merging.jl");
-include("generating.jl");
-include("mutating.jl");
+include("generation.jl");
+include("mutation.jl");
 include("literal_optimization.jl");
 
 export mutate, reproduce, _cw_, CandidateFunction, AlgorithmParameters, AllowedOperationDescription, init, swap_literals_with_params, optimize_literals!
