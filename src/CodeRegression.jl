300df12c97497a6b47456c624ca6d96e8bac6c3a
ejlectron <ish.myk0la@gmail.com>
2026-08-04 10:05:07 +0200

    Refactored name of directories and included all structural changes in imports/exports

--- src/CodeRegression.jl
+++ src/CodeRegression.jl
@@ -9,21 +9,45 @@ module Operators
     module Utils
         include("Operators/Utils/expressions.jl")
         include("Operators/Utils/utils.jl")
+        include("Operators/Utils/visitor_template.jl")
     end
 
     module Methods
         module Generation
+            module Core
+
+            end
+
+            module Operations
+
+            end
+
             module GP
                 module Operators
-                    include("Operators/Methods/Generation/GP/Operations/generate_expressions.jl")
+                    include("Operators/Methods/Generation/GP/operations/generate_expressions.jl")
                 end
             end
         end
 
         module Literal
-            include("Operators/Methods/Literal/literal_visitor.jl")
-            include("Operators/Methods/Literal/visitor_template.jl")
-            include("Operators/Methods/Literal/literal_optimization.jl")
+            module Core
+                
+            end
+
+            module Operations
+                include("Operators/Methods/Literal/Operations/literal_optimization.jl")
+                include("Operators/Methods/Literal/Operations/literal_visitor.jl")
+            end
+        end
+
+        module Merge
+            module Core
+                
+            end
+
+            module Operations
+                include("Operators/Methods/Merge/Operations/merging.jl")
+            end
         end
 
         module Mutation
@@ -31,15 +55,9 @@ module Operators
                 include("Operators/Methods/Mutation/core/mutation_core.jl")
             end
 
-            module MergingAlgo
-                module Operators
-                    include("Operators/Methods/Mutation/merging_algo/Operations/merging.jl")
-                end
-            end
-
             module MutationAlgo
                 module Operators
-                    include("Operators/Methods/Mutation/mutation_algo/Operations/mutation.jl")
+                    include("Operators/Methods/Mutation/mutation_algo/operations/mutation.jl")
                 end
             end
         end
@@ -52,11 +70,12 @@ end
 
 import .Operators.Utils: AllowedOperationDescription, AlgorithmParameters, CandidateFunction, ConstsAndExprs, _cw_, make_const_wrap, init, generate_callables!, check_expr_type, get_body, get_signature, get_symbol_type_pair, get_args, get_arg_types, general_type, get_return_type
 import .Operators.Utils: _cw_, make_const_wrap
-import .Operators.Methods.Literal: ParamLiteralCandidateFunction, swap_literals_with_params, optimize_literals!, find_literals
-import .Operators.Methods.Mutation.MergingAlgo.Operators: reproduce
+import .Operators.Methods.Generation.GP.Operators: generate_exprs, generate_op!, generate_stmt, generate_assignment, generate_return, generate_block, generate_if
+
+import .Operators.Methods.Literal.Operations: ParamLiteralCandidateFunction, swap_literals_with_params, optimize_literals!, find_literals
+import .Operators.Methods.Merge.Operations: reproduce
 import .Operators.Methods.Mutation.MutationAlgo.Operators: mutate, mutate_pure_expression
 import .Operators.Methods.Mutation.Core: __MutationContext
-import .Operators.Methods.Generation.GP.Operators: generate_exprs, generate_op!, generate_stmt, generate_assignment, generate_return, generate_block, generate_if
 
 export mutate, reproduce, _cw_, CandidateFunction, AlgorithmParameters, AllowedOperationDescription, init, swap_literals_with_params, optimize_literals!, generate_callables!, generate_exprs, generate_op!, generate_stmt, generate_assignment, generate_return, generate_block, generate_if, ConstsAndExprs, make_const_wrap, find_literals, ParamLiteralCandidateFunction, mutate_pure_expression
 
